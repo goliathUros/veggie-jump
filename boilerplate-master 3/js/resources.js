14208aa168518934398086c0152667d91f588055
goliathUroborrows <djiwoonyi@gmail.com>
2014-03-11 17:43:19 -0700

    whatever
    game

--- boilerplate-master 3/js/resources.js
+++ boilerplate-master 3/js/resources.js
@@ -4,6 +4,7 @@ game.resources = [
 	 * @example
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
+	 {name: "wow lily", type:"image", src:"data/img/map/kitchen.png"}
 
 	/* Atlases 
 	 * @example
@@ -15,6 +16,7 @@ game.resources = [
 	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
 	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
  	 */
+ 	 {name: "area01", type:"tmx", src:"data/map/area01.tmx" }
 
 	/* Background music. 
 	 * @example
